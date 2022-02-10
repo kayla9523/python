2f3a1f9e9fae5483fff8058d265bc19f98d8de80
kayla9523 <71369184+kayla9523@users.noreply.github.com>
2022-02-10 13:29:29 +0900

    Add files via upload

---
+++ 각 달의 일수 세기.py
@@ -0,0 +1,8 @@
+a = int(input("1부터 12사이의 정수를 쓰시오"))
+if a == 1 or a== 3 or a == 5 or a == 7 or a == 8 or a == 10 or a == 12:
+        print(a,"는 31일까지")
+
+if a == 2:
+        print(a,"는 28일까지")
+if a == 4 or a == 6 or a == 9 or a == 11:
+        print(a,"는 30일까지")
